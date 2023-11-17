736b7021ee83e90ea06647eb8e10eb7a4a43e476
Saliim242 <github242A@gmail.com>
2023-11-17 10:58:06 +0300

    Themes

---
+++ lib/utils/theme/customs/custom_exports.dart
@@ -0,0 +1,2 @@
+export 'custom_text_theme.dart';
+export 'eleveted_button_theme.dart';
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'customs/custom_exports.dart';
+
+class TAppTheme {
+  TAppTheme._();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevetedButtonTheme.lightElevatedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevetedButtonTheme.darkElevatedButtonTheme,
+  );
+}
